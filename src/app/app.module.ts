026b58f5ff001e2aa2583b72042b173a3382107a
wachuuu <gab.wachowski15@gmail.com>
2021-05-23 20:49:57 +0200

    added main container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BackgroundComponent } from './components/background/background.component';
+import { ContainerComponent } from './components/container/container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BackgroundComponent
+    BackgroundComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule,
